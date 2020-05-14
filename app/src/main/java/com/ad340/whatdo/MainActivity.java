959deb670be4a41957f3db7e938c473f79b91334
Lenny Brown <penn2014@gmail.com>
2020-05-14 15:09:32 -0700

    Added recycler view to main activity.

--- app/src/main/java/com/ad340/whatdo/MainActivity.java
+++ app/src/main/java/com/ad340/whatdo/MainActivity.java
@@ -1,7 +1,14 @@
 package com.ad340.whatdo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.text.Layout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView toDoRecyclerView = findViewById(R.id.todo_list_recycler_view);
+        toDoRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<ToDoItem> toDoList = new ArrayList<>();
+        toDoList.add(new ToDoItem("Test"));
+        toDoList.add(new ToDoItem("Test"));
+        toDoList.add(new ToDoItem("Test"));
+        toDoList.add(new ToDoItem("Test"));
+        toDoList.add(new ToDoItem("Test"));
+
+        ToDoItemRecyclerViewAdapter adapter = new ToDoItemRecyclerViewAdapter(toDoList);
+        toDoRecyclerView.setAdapter(adapter);
+        int largePadding = getResources().getDimensionPixelSize(R.dimen.large_item_spacing);
+        int smallPadding = getResources().getDimensionPixelSize(R.dimen.small_item_spacing);
+        toDoRecyclerView.addItemDecoration(new ToDoItemDecoration(largePadding, smallPadding));
     }
 }
