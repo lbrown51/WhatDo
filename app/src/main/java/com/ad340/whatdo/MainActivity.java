f775c11ca62cd67cfb430b63fbf5e3e5690c0fef
Larissa <lyaholkovsky@gmail.com>
2020-05-14 23:32:31 -0700

    date displaying correctly

--- app/src/main/java/com/ad340/whatdo/MainActivity.java
+++ app/src/main/java/com/ad340/whatdo/MainActivity.java
@@ -6,13 +6,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.text.Layout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private List<ToDoItem> toDoList;
+    private TextView dateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +37,13 @@ public class MainActivity extends AppCompatActivity {
         int largePadding = getResources().getDimensionPixelSize(R.dimen.large_item_spacing);
         int smallPadding = getResources().getDimensionPixelSize(R.dimen.small_item_spacing);
         toDoRecyclerView.addItemDecoration(new ToDoItemDecoration(largePadding, smallPadding));
+
+        dateView = findViewById(R.id.todays_date);
+        Calendar today = Calendar.getInstance();
+        dateView.setText(
+                String.format("%02d - %02d - %04d",
+                        today.get(Calendar.DAY_OF_MONTH),
+                        today.get(Calendar.MONTH ) + 1,
+                        today.get(Calendar.YEAR)));
     }
 }
