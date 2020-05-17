f89a9dc7f007700fe6321af7f197a50bf7064948
dsjack <dsjack89@gmail.com>
2020-05-17 14:20:49 -0700

    Makes todos expandable
    * Brings ViewHolder into ViewAdapter. Had to do this because ViewHolder needs the toDoList variable from ViewAdapter and the ViewAdapter's notifyItemChanged methods

--- app/src/main/java/com/ad340/whatdo/ToDoItemRecyclerViewAdapter.java
+++ app/src/main/java/com/ad340/whatdo/ToDoItemRecyclerViewAdapter.java
@@ -3,14 +3,15 @@ package com.ad340.whatdo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class ToDoItemRecyclerViewAdapter
-        extends RecyclerView.Adapter<ToDoItemViewHolder> {
+public class ToDoItemRecyclerViewAdapter extends RecyclerView.Adapter<ToDoItemRecyclerViewAdapter.ToDoItemViewHolder> {
     private List<ToDoItem> toDoList;
 
     ToDoItemRecyclerViewAdapter(List<ToDoItem> toDoList) {
@@ -31,6 +32,9 @@ public class ToDoItemRecyclerViewAdapter
         if (toDoList != null && position < toDoList.size()) {
             ToDoItem toDo = toDoList.get(position);
             holder.toDoTaskName.setText(toDo.taskName);
+
+            boolean isExpanded = toDoList.get(position).isExpanded();
+            holder.todoDetail.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
         }
 
     }
@@ -38,5 +42,25 @@ public class ToDoItemRecyclerViewAdapter
     @Override
     public int getItemCount() {
         return toDoList.size();
+    }
+
+    public class ToDoItemViewHolder extends RecyclerView.ViewHolder {
+        public TextView toDoTaskName;
+        ConstraintLayout todoDetail;
+
+        public ToDoItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            toDoTaskName = itemView.findViewById(R.id.todo_item_task_name);
+            todoDetail = itemView.findViewById(R.id.todo_detail);
+
+            toDoTaskName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ToDoItem toDoItem = toDoList.get(getAdapterPosition());
+                    toDoItem.setExpanded(!toDoItem.isExpanded());
+                    notifyItemChanged(getAdapterPosition()); // calls onBindViewHolder for position
+                }
+            });
+        }
     }
 }
