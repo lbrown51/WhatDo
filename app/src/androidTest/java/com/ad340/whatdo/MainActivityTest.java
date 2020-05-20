c04c49d9ae5f54bf60cbf8dffc599677d4f408ea
unicornsleuth <57076614+unicornsleuth@users.noreply.github.com>
2020-05-20 14:04:26 -0700

    Issue 18 fab transition create (#20)
    * changed button slightly, added button functionality
    
    * renamed branch to reflect issue
    
    * switched to ImageButton for styling - onClick wouldn't work - switched back to FAB
    
    * fab opens dialog
    
    * fixed close button, nixed animations
    
    * clean up code
    
    * add testing
    
    * last commit before PR (ideally)

--- app/src/androidTest/java/com/ad340/whatdo/MainActivityTest.java
+++ app/src/androidTest/java/com/ad340/whatdo/MainActivityTest.java
@@ -3,6 +3,7 @@ package com.ad340.whatdo;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -21,10 +22,12 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.Matchers.not;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -108,6 +111,27 @@ public class MainActivityTest {
     public void hasFloatingActionButton() {
         onView(withId(R.id.fab))
                 .check(matches(isDisplayed()));
+    }
+
+    /*
+        Tests whether the floating action button opens a dialog on click,
+        and the x button in the dialog closes the dialog
+     */
+    @Test
+    public void openCloseDialog() {
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.create_todo_dialog))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.close_dialog)).perform(click());
+        try {
+            onView(withId(R.id.create_todo_dialog))
+                    .check(matches(not(isDisplayed())));
+            onView(withId(R.id.placeholder_text))
+                    .check(matches(withText(R.string.placeholder_text)));
+        } catch (NoMatchingViewException e) {
+            onView(withId(R.id.fab))
+                    .check(matches(isDisplayed()));
+        }
     }
 
     /*
--- app/src/main/java/com/ad340/whatdo/MainActivity.java
+++ app/src/main/java/com/ad340/whatdo/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Layout;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.Window;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -19,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     private List<ToDoItem> toDoList;
     private MaterialToolbar header;
+    public FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +55,11 @@ public class MainActivity extends AppCompatActivity {
         int smallPadding = getResources().getDimensionPixelSize(R.dimen.small_item_spacing);
         toDoRecyclerView.addItemDecoration(new ToDoItemDecoration(largePadding, smallPadding));
 
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+                showCreateDialog();
+            });
+
         Calendar today = Calendar.getInstance();
         header = findViewById(R.id.top_app_bar);
         StringBuilder displayText = new StringBuilder(header.getTitle());
@@ -52,5 +71,28 @@ public class MainActivity extends AppCompatActivity {
                 today.get(Calendar.DAY_OF_MONTH),
                 today.get(Calendar.YEAR)));
         header.setTitle(displayText);
-    }
+}
+
+    private void showCreateDialog() {
+        final View createView = View.inflate(this, R.layout.create_todo_dialog, null);
+
+        final Dialog dialog = new Dialog(this, android.R.style.Theme_DeviceDefault_NoActionBar_Overscan);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(createView);
+
+        ImageButton closeButton = (ImageButton) dialog.findViewById(R.id.close_dialog);
+        closeButton.setOnClickListener((view) -> {
+            view.setVisibility(View.INVISIBLE);
+            dialog.dismiss();
+        });
+
+        dialog.setOnKeyListener((dialogInterface, key, keyEvent) -> {
+            if (key == KeyEvent.KEYCODE_BACK) {
+                dialogInterface.dismiss();
+                return true;
+            } else return false;
+        });
+
+        dialog.show();
+    };
 }
