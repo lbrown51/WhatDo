03c28ab3d3d17773d4be7f723ea5f085a1f57b50
dsjack <dsjack89@gmail.com>
2020-05-26 23:53:27 -0700

    Adds unit test of getCompleted() method

---
+++ app/src/test/java/com/ad340/whatdo/MainActivityUnitTests.java
@@ -0,0 +1,14 @@
+package com.ad340.whatdo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityUnitTests {
+
+    @Test
+    public void get_completedWorks() {
+        Todo todo = new Todo(null, "Test todo", null, null, null);
+        assertFalse(todo.getCompleted());
+    }
+}
