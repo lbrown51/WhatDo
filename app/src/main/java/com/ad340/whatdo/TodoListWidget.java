f00ad5323c926c7f9093ca271a06c563440a56da
Lenny Brown <penn2014@gmail.com>
2020-06-03 13:25:39 -0700

    Widget now displays list

--- app/src/main/java/com/ad340/whatdo/TodoListWidget.java
+++ app/src/main/java/com/ad340/whatdo/TodoListWidget.java
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -18,6 +19,7 @@ public class TodoListWidget extends AppWidgetProvider {
 
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.todo_list_widget);
+        setRemoteAdapter(context, views);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -29,21 +31,25 @@ public class TodoListWidget extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
+
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+        Toast.makeText(context,"onEnabled called", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+        Toast.makeText(context,"onDisabled called", Toast.LENGTH_LONG).show();
     }
 
     private static void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
         views.setRemoteAdapter(R.id.todo_widget_list,
-                new Intent(context, TodoWidgetService.class));
+                new Intent(context, TodoListWidgetService.class));
     }
 }
 
